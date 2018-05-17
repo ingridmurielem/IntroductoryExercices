079c61e70ca7d60efb8a2d7735f2eedd97bce6e4
ingrid <murielem.ingrid@gmail.com>
2018-05-17 13:58:38 -0300

    creating project

---
+++ src/TriangleExercises.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class TriangleExercices {
+
+    private static final String ASTERISK = "*";
+    public static void PrintsANumberOfAsterisksHorizontalLine( int number){
+        for (int i = 0; i<number; i++){
+            System.out.print(ASTERISK);
+        }
+    }
+    public static void PrintsANumberOfAsterisksVerticalLine( int number){
+        for (int i = 0; i<number; i++){
+            System.out.println(ASTERISK);
+        }
+    }
+    public static void PrintOneAsterisks (){
+        System.out.print(ASTERISK);
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+}
